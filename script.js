cc15592136e1ec16fad38d2dd35b5e74089b636c
by-tess <livrierot@gmail.com>
2022-03-19 16:14:24 +0100

    push files

---
+++ script.js
@@ -0,0 +1,9 @@
+function showHideSignin() {
+    let signin = document.getElementById('signin');
+
+    if(signin.style.display == "block") {
+        signin.style.display = "none";
+    } else {
+        signin.style.display = "block";
+    }
+}

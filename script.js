691bdb7329217664b36f8f6cc43a86516fb64f83
nicholas pesaresi <nicholaspesaresi@gmail.com>
2022-04-01 10:01:57 +0200

    js update

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* ROTATE ICONS */
+
 const menu = document.getElementById("ham-svg")
 
 function rotate(){
@@ -10,3 +12,46 @@ function rotate(){
 document.getElementById("ham-svg").addEventListener("click", rotate)
 
 document.getElementById("search-svg").addEventListener("click", rotate)
+
+
+const navmenu = document.getElementById("navmenu");
+
+
+/* SHOW MENU' */
+
+function showNavMenu(){
+   document.getElementById("navmenu").style.display = "flex"
+   
+}
+document.getElementById("ham-svg").addEventListener("click", showNavMenu)
+
+/* HIDE MENU' */
+
+function hideNavMenu(){
+    document.getElementById("navmenu").style.display = "none"
+     
+ }
+ document.getElementById("exitmenu").addEventListener("click", hideNavMenu)
+
+/* SEARCHBAR MENU */
+
+const search = document.getElementById("search-svg")
+
+/*SHOW SEARCHBAR MENU' */
+
+function showSearchMenu(){
+    document.getElementById("searchmenu").style.display = "flex"
+    document.getElementById("ham-svg").style.display = "none"
+    
+ }
+ document.getElementById("search-svg").addEventListener("click", showSearchMenu)
+
+/* HIDE SEARCHBAR MENU' */
+
+function hideSearchMenu(){
+    document.getElementById("searchmenu").style.display = "none"
+     
+ }
+ document.getElementById("exit-searchmenu").addEventListener("click", hideSearchMenu)
+
+
